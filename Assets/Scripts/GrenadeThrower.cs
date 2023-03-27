7d8856fcb8088194eba8660bb1a543884474c55a
SimonH0202 <simon.hoeglinger@gmail.com>
2023-03-27 21:02:20 +0200

    Changed GrenadeThrower script to new input system

--- Assets/Scripts/GrenadeThrower.cs
+++ Assets/Scripts/GrenadeThrower.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GrenadeThrower : MonoBehaviour
 {
@@ -13,7 +14,7 @@ public class GrenadeThrower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("g"))
+        if (Keyboard.current.gKey.wasPressedThisFrame)
         {
             ThrowGrenade();
         }
