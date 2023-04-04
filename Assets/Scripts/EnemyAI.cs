1f3e499d8cf3694d2213de10a084b20f20ad1b67
Jakob2266 <s2110238041@fhooe.at>
2023-04-04 18:18:55 +0200

    Changed EnemyAI to abstract class and added LightEnemy Class

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -3,54 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyAI : MonoBehaviour
-{
-    
+public abstract class EnemyAI : MonoBehaviour
+{ 
     //Editor Enemy Settings
-    [SerializeField] private Transform[] patrolPoints;
-    [SerializeField] private List<EnemyAI> fellowAI;
-    [SerializeField] private Transform[] chaseSpots;
-
-    [Header("Enemy Stats")]
+    [SerializeField] protected Transform[] patrolPoints;
+    [SerializeField] protected List<EnemyAI> fellowAI;
+  
     //Enemy stats
-    [SerializeField] private float health = 100f;
+    [SerializeField] protected float health = 100f;
 
-    [SerializeField] private StatsBar healthBar;
+    [SerializeField] protected StatsBar healthBar;
 
-    [Header("Enemy Attack Settings")]
+
     //Enemy attack settings
-    [SerializeField] private float attackDamage = 10f;
+    [SerializeField] protected float attackDamage = 10f;
 
-    [SerializeField] private float weaponLength = 2f;
-
-    [SerializeField] private Transform weaponPoint;
-
-    [SerializeField] private LayerMask playerLayers;
 
     //Private patrolling variables
-    private int currentPoint;
-    private bool patrolling;
-    private bool attacking;
-    private bool hit;
-
-
-    private List<GameObject> damagedPlayers = new List<GameObject>();
+    protected int currentPoint;
+    protected bool patrolling;
+    protected bool attacking;
+    protected bool hit;
 
     //Private enemy variables
-    private bool dead = false;
+    protected bool dead = false;
 
     //References
     private MovementController playerController;
-    private NavMeshAgent agent;
-    private GameObject player;
-    private Animator animator;
+    protected NavMeshAgent agent;
+    protected GameObject player;
+    protected Animator animator;
 
     //Animation hashes
-    int isWalkingHash;
-    int isAttackingHash;
-    int isDeadHash;
+    protected int isWalkingHash;
+    protected int isAttackingHash;
+    protected int isDeadHash;
 
-    private void Start()
+
+    protected void Start()
     {
         //Get References
         agent = GetComponent<NavMeshAgent>();
@@ -69,63 +59,15 @@ public class EnemyAI : MonoBehaviour
         //Set up start destination
         agent.destination = patrolPoints[currentPoint].position;
 
-        //Set up animation hashes
         isWalkingHash = Animator.StringToHash("isWalking");
         isAttackingHash = Animator.StringToHash("isAttacking");
         isDeadHash = Animator.StringToHash("isDead");
+
     }
+      
 
-    private void Update()
-    {
-        if (health <= 0) Die();
-        if (!dead)
-        {
-            PatrolAndAttack();
-            HandleWalkAnimation();
-            AttackIfHit();
-        }
-    }
-
-    public void PatrolAndAttack()
-    {
-        if(patrolling){
-            //Player is in sight
-             if(Vector3.Distance(this.transform.position, player.transform.position) <= 10f)
-            {
-                FollowPlayer();
-            }
-            //Player is out of sight
-            if(Vector3.Distance(this.transform.position, player.transform.position) > 10f)
-            {   
-                agent.speed = 5f;
-                agent.destination = patrolPoints[currentPoint].position;
-            }
-            //Go to next waypoint
-            if(Vector3.Distance(this.transform.position, patrolPoints[currentPoint].position) <= 5f)
-            {
-                Iterate(); 
-            }    
-        }
-        //Check if player is still in reach
-        if(!patrolling)
-        {
-            if(CheckDistance()) FollowPlayer();
-            else patrolling = true;
-
-            //Player is in attack range
-            if(Vector3.Distance(this.transform.position, player.transform.position) <= 2f)
-            {
-                if (!attacking)
-                {
-                    attacking = true;
-                    agent.isStopped = true;
-                    StartCoroutine(Attack());
-                }
-                HandleDamageRaycast();
-            }
-        }       
-    }
-
+    public abstract void PatrolAndAttack();
+    
     public void AttackIfHit()
     {
         if (hit && patrolling)
@@ -138,7 +80,6 @@ public class EnemyAI : MonoBehaviour
         }
     }
     
-
     public void TakeDamage(float damage)
     {
             health -= damage;
@@ -146,56 +87,7 @@ public class EnemyAI : MonoBehaviour
             hit = true;
     }
 
-    void Die()
-    {   
-        dead = true;
-
-        //Remove enemy from list
-        fellowAI.Remove(this);
-
-        for(int i = 0; i < fellowAI.Count; i++)
-        {
-            //Set all fellow AI to follow player on death
-            fellowAI[i].FollowPlayer();
-
-            //Remove this enemy from fellow AI list
-            fellowAI[i].fellowAI.Remove(this);
-        }
-
-        //Disable enemy
-        Destroy(GetComponent<Collider>());
-
-        //Disable navmesh agent movement
-        agent.SetDestination(transform.position);
-
-        //Disable health bar
-        healthBar.gameObject.SetActive(false);
-
-        //Set dead animation
-        animator.SetBool(isDeadHash, true);
-        animator.SetBool(isWalkingHash, false);
-        animator.SetBool(isAttackingHash, false);
-
-        //Delete enemy after 10 seconds
-        Destroy(gameObject, 10f);
-    }
-
-    //Attack coroutine
-    IEnumerator Attack()
-    {
-        //Play attack animation
-        animator.SetBool(isAttackingHash, true);
-
-        //Wait for animation to finish
-        yield return new WaitForSeconds(1f);
-        animator.SetBool(isAttackingHash, false);
-
-        //Wait for a bit and reset attacking
-        yield return new WaitForSeconds(Random.Range(0.5f, 1f));
-        attacking = false;
-        agent.isStopped = false;
-        damagedPlayers.Clear();
-    }
+    public abstract void Die();
 
     //Stop following coroutine
     IEnumerator StopFollowing()
@@ -205,57 +97,9 @@ public class EnemyAI : MonoBehaviour
         patrolling = true;
     }
 
-    void HandleWalkAnimation()
-    {
-        //Handle walk animation and rotation
-        if (agent.velocity.magnitude > 0.5f)
-        {
-            animator.SetBool(isWalkingHash, true);
-            if (patrolling) transform.rotation = Quaternion.LookRotation(agent.velocity.normalized);
-            else transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.transform.position - transform.position), 5f * Time.deltaTime);
-        }
-        else
-        {
-            animator.SetBool(isWalkingHash, false);
-            if (!patrolling) transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z));
-        }       
-    }
 
-    void FollowPlayer()
-    {
-        int k = 0;
-        for(int i = 0; i < fellowAI.Count; i++)
-        {
-            fellowAI[i].patrolling = false;
-            fellowAI[i].agent.speed = 7.5f;
-            fellowAI[i].agent.destination = chaseSpots[k].position;
-            k++;
-            // out of bounds error handling, spare enemys share first spot
-            if(k >= chaseSpots.Length)
-            {
-                k = 0;
-            }
-        }
-    }
-
-    void HandleDamageRaycast()
-    {
-        if (attacking)
-        {
-            RaycastHit hit;
-
-            //Check for enemies hit by raycast
-            if (Physics.Raycast(weaponPoint.position, -weaponPoint.transform.up, out hit, weaponLength, playerLayers))
-            {
-                if (!damagedPlayers.Contains(hit.transform.gameObject))
-                {
-                    //Damage enemy
-                    damagedPlayers.Add(hit.transform.gameObject);
-                    hit.transform.GetComponent<PlayerStatsSystem>().TakeDamage(attackDamage);
-                }
-            }
-        }
-    }
+    public abstract void FollowPlayer();
+   
 
     public bool CheckDistance(){
         //Check if player is in reach of one of the enemys
@@ -269,8 +113,7 @@ public class EnemyAI : MonoBehaviour
         return false;
     }
 
-
-    void Iterate()
+    protected void Iterate()
     {
         //Iterate to next waypoint
         if(currentPoint < patrolPoints.Length-1)
@@ -286,9 +129,5 @@ public class EnemyAI : MonoBehaviour
     
     public bool Patrolling { get; set; }
 
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawLine(weaponPoint.position, weaponPoint.position + weaponLength * -weaponPoint.transform.up);
-    }
+  
 }
---
+++ Assets/Scripts/LightEnemy.cs
@@ -0,0 +1,180 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightEnemy : EnemyAI
+{
+   
+    [SerializeField] private Transform[] chaseSpots;
+
+    [SerializeField] private float weaponLength = 2f;
+
+    [SerializeField] private Transform weaponPoint;
+
+    [SerializeField] private LayerMask playerLayers;
+
+    private List<GameObject> damagedPlayers = new List<GameObject>();
+
+  
+    private void Update()
+    {
+        if (health <= 0) Die();
+        if (!dead)
+        {
+            PatrolAndAttack();
+            HandleWalkAnimation();
+            AttackIfHit();
+        }
+    }
+
+    public override void PatrolAndAttack()
+    {
+        if(patrolling){
+            //Player is in sight
+             if(Vector3.Distance(this.transform.position, player.transform.position) <= 10f)
+            {
+                FollowPlayer();
+            }
+            //Player is out of sight
+            if(Vector3.Distance(this.transform.position, player.transform.position) > 10f)
+            {   
+                agent.speed = 5f;
+                agent.destination = patrolPoints[currentPoint].position;
+            }
+            //Go to next waypoint
+            if(Vector3.Distance(this.transform.position, patrolPoints[currentPoint].position) <= 5f)
+            {
+                Iterate(); 
+            }    
+        }
+        //Check if player is still in reach
+        if(!patrolling)
+        {
+            if(CheckDistance()) FollowPlayer();
+            else patrolling = true;
+
+            //Player is in attack range
+            if(Vector3.Distance(this.transform.position, player.transform.position) <= 2f)
+            {
+                if (!attacking)
+                {
+                    attacking = true;
+                    agent.isStopped = true;
+                    StartCoroutine(Attack());
+                }
+                HandleDamageRaycast();
+            }
+        } 
+    }
+
+    public override void Die()
+    {
+        //Remove enemy from list
+        fellowAI.Remove(this);
+
+        for(int i = 0; i < fellowAI.Count; i++)
+        {
+            LightEnemy lightEnemy = fellowAI[i] as LightEnemy;
+            //Set all fellow AI to follow player on death
+            lightEnemy.FollowPlayer();
+
+            //Remove this enemy from fellow AI list
+            lightEnemy.fellowAI.Remove(this);
+        }
+
+        //Disable enemy
+        dead = true;
+        Destroy(GetComponent<Collider>());
+
+        //Disable navmesh agent movement
+        agent.SetDestination(transform.position);
+
+        //Disable health bar
+        healthBar.gameObject.SetActive(false);
+
+        //Set dead animation
+        animator.SetBool(isDeadHash, true);
+        animator.SetBool(isWalkingHash, false);
+        animator.SetBool(isAttackingHash, false);
+
+        //Delete enemy after 10 seconds
+        Destroy(gameObject, 10f);
+    }
+
+      //Attack coroutine
+    IEnumerator Attack()
+    {
+        //Play attack animation
+        animator.SetBool(isAttackingHash, true);
+
+        //Wait for animation to finish
+        yield return new WaitForSeconds(1f);
+        animator.SetBool(isAttackingHash, false);
+
+        //Wait for a bit and reset attacking
+        yield return new WaitForSeconds(Random.Range(0.5f, 1f));
+        attacking = false;
+        agent.isStopped = false;
+        damagedPlayers.Clear();
+    }
+
+    void HandleWalkAnimation()
+    {
+        //Handle walk animation and rotation
+        if (agent.velocity.magnitude > 0.5f)
+        {
+            animator.SetBool(isWalkingHash, true);
+            if (patrolling) transform.rotation = Quaternion.LookRotation(agent.velocity.normalized);
+            else transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.transform.position - transform.position), 5f * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetBool(isWalkingHash, false);
+            if (!patrolling) transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z));
+        }       
+    }
+
+    public override void FollowPlayer()
+    {
+        int k = 0;
+        for(int i = 0; i < fellowAI.Count; i++)
+        {
+            LightEnemy lightEnemy = fellowAI[i] as LightEnemy;
+            lightEnemy.patrolling = false;
+            lightEnemy.agent.speed = 7.5f;
+            lightEnemy.agent.destination = chaseSpots[k].position;
+            k++;
+            // out of bounds error handling, spare enemys share first spot
+            if(k >= chaseSpots.Length)
+            {
+                k = 0;
+            }
+        }
+    }
+
+    void HandleDamageRaycast()
+    {
+        if (attacking)
+        {
+            RaycastHit hit;
+
+            //Check for enemies hit by raycast
+            if (Physics.Raycast(weaponPoint.position, -weaponPoint.transform.up, out hit, weaponLength, playerLayers))
+            {
+                if (!damagedPlayers.Contains(hit.transform.gameObject))
+                {
+                    //Damage enemy
+                    damagedPlayers.Add(hit.transform.gameObject);
+                    hit.transform.GetComponent<PlayerStatsSystem>().TakeDamage(attackDamage);
+                }
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(weaponPoint.position, weaponPoint.position + weaponLength * -weaponPoint.transform.up);
+    }
+
+}
